29d87eeb8e3435e6d39ec4a1cb6bfac9d9cc0eaa
redass12 <redataraoui1@gmail.com>
2020-03-19 19:48:11 +0100

    initial commit

---
+++ precache-manifest.8f9e449141a01093f3f2da3076142ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb2a82d3403896b857baf493abfa8c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5134ddd6437edef1a20",
+    "url": "/static/css/main.86985aa5.chunk.css"
+  },
+  {
+    "revision": "494984a6ee3657ae3716",
+    "url": "/static/js/2.881103e6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.881103e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5134ddd6437edef1a20",
+    "url": "/static/js/main.1ce85af4.chunk.js"
+  },
+  {
+    "revision": "ccd1adbe065bbf296b21",
+    "url": "/static/js/runtime-main.7c7f9f47.js"
+  }
+]);
